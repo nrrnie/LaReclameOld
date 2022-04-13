3db25e0cd11ace11fd84e5c02a9cce5263ac93e3
mercurial <dias.nespayev@gmail.com>
2022-04-14 00:42:03 +0600

    feat: register users

--- la_reclame/auth/routes.py
+++ la_reclame/auth/routes.py
@@ -1,9 +1,20 @@
 from flask import request, render_template
 
 from la_reclame.auth import auth
+from utils import api
 
 
 @auth.route('/registration', methods=['GET', 'POST'])
 def registration():
     if request.method == 'GET':
         return render_template('registration.html')
+
+    username = request.form.get('username', '')
+    password = request.form.get('password', '')
+    email = request.form.get('email', '')
+
+    response = api.register_user(username, email, password)
+    if response == 'ok':
+        return 'registered'
+
+    return render_template('registration.html')
