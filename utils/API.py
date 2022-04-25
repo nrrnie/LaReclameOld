66bf1f3e3c840ec2e59cc1c8bd01a90c8ca75d8c
mercurial <dias.nespayev@gmail.com>
2022-04-25 13:12:47 +0600

    fix: api requests

--- utils/API.py
+++ utils/API.py
@@ -8,49 +8,37 @@ class API:
         self.host = 'http://127.0.0.1:5001'
 
     def register_user(self, username: str, email: str, password: str) -> str:
-        url = self.host + '/auth/add/user'
+        link = self.host + '/auth/add/user'
         data = dict(username=username, email=email, password=password)
-        try:
-            response = post(url, data=data).json()
-        except RequestException:
-            return 'Request error'
-
-        if response['status'] == 'ok':
-            return 'ok'
-        return response['error']
+        response = API.safe_request(link, data=data)
+        return response if response['status'] == 'error' else response['response']
 
     def login(self, username: str, password: str) -> str:
-        url = self.host + '/auth/login'
+        link = self.host + '/auth/login'
         data = dict(username=username, password=password)
-
-        try:
-            response = post(url, data=data).json()
-        except RequestException:
-            return 'Request error'
-
-        if response['status'] == 'ok':
-            return 'ok'
-
-        return response['error']
+        response = API.safe_request(link, data=data)
+        return response if response['status'] == 'error' else response['response']
 
     def find_user_by_id(self, user_id: int) -> dict:
         link = self.host + '/users/user_by_id/{}'.format(user_id)
         response = API.safe_request(link)
-        return response if response['status'] == 'error' else response
+        return response if response['status'] == 'error' else response['response']
 
     def find_user_by_username(self, user_name: str) -> dict:
         link = self.host + '/users/user_by_username/{}'.format(user_name)
         response = API.safe_request(link)
-        return response if response['status'] == 'error' else response
+        return response if response['status'] == 'error' else response['response']
 
     def find_user_by_email(self, user_email: str) -> dict:
         link = self.host + '/users/user_by_email/{}'.format(user_email)
         response = API.safe_request(link)
-        return response if response['status'] == 'error' else response
+        return response if response['status'] == 'error' else response['response']
 
     @staticmethod
     def safe_request(link: str, data: dict = None) -> dict:
         try:
             response = post(link, data=data).json()
         except RequestException:
-            return {'status': 'error', 'response': response}
+            return dict(status='error', error='API error')
+
+        return dict(status='ok', response=response)
